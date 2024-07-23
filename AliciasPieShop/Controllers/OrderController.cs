40842c65df8cbb40f97d09175f9a1bb845bc918b
Alicia Smith <Alicia.Smith@gateleyplc.com>
2024-07-23 11:51:25 +0100

    added checkout view which contains our form. added OrderController with Order/Checkout endpoint which returns the checkout view. added button to ShoppingCart index view which navigates to checkout endpoint

---
+++ AliciasPieShop/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using AliciasPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AliciasPieShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IShoppingCart _shoppingCart;
+
+        public OrderController(IShoppingCart shoppingCart, IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
